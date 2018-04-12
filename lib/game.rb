ee0f647322436e4426548de92decb2d3c3a73d54
Trevor Seitz <trevinla@gmail.com>
2018-04-12 19:18:50 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -8,7 +8,7 @@ attr_accessor :player_1, :player_2, :board
 [2, 4, 6], [0, 3, 6],
 [1, 4, 7], [2, 5, 8],
 ]
- 
+
 def initialize(player_1 = Players::Human.new("X") , player_2 = Players::Human.new("O"), board = Board.new)
   @player_1 = player_1
   @player_2 = player_2
